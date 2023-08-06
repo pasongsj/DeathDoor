dc644567ed8c8cf112f436602e50ead7c9a8c259
동찬 황 <shidge98@gmail.com>
2023-08-06 18:42:26 +0900

    황동찬 add : 용량큰 lib 압축 자동해제 배치파일 추가

---
+++ Unzip.bat
@@ -0,0 +1,8 @@
+
+powershell expand-archive GameEngineCore\ThirdParty\FBX\lib\x64\Debug\libfbxsdk-md.zip GameEngineCore\ThirdParty\FBX\lib\x64\Debug\
+
+powershell expand-archive GameEngineCore\ThirdParty\FBX\lib\x64\Debug\libfbxsdk-mt.zip GameEngineCore\ThirdParty\FBX\lib\x64\Debug\
+
+powershell expand-archive GameEngineCore\ThirdParty\FBX\lib\x64\Release\libfbxsdk-md.zip GameEngineCore\ThirdParty\FBX\lib\x64\Release\
+
+powershell expand-archive GameEngineCore\ThirdParty\FBX\lib\x64\Release\libfbxsdk-mt.zip GameEngineCore\ThirdParty\FBX\lib\x64\Release\
