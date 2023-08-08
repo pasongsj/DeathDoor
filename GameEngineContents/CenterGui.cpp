1dd1100ab4f028f0dc5d743c744db14488bf5470
동찬 황 <shidge98@gmail.com>
2023-08-08 22:29:40 +0900

    황동찬　Ａｄｄ　저장　안해서　커밋안된것　추가　업로드

--- GameEngineContents/CenterGui.cpp
+++ GameEngineContents/CenterGui.cpp
@@ -1,11 +1,31 @@
 #include "PrecompileHeader.h"
 #include "CenterGui.h"
 
+#include "CenterLevel.h"
+#include "TestLevel.h"
+
 CenterGui::CenterGui() 
 {
 }
 
 CenterGui::~CenterGui() 
 {
+}
+
+void CenterGui::Start()
+{
+}
+
+void CenterGui::OnGUI(std::shared_ptr<class GameEngineLevel> Level, float _DeltaTime)
+{
+	if (ImGui::Button("CenterLevel") && Level.get() != GetLevel())
+	{
+		GameEngineCore::ChangeLevel("CenterLevel");
+	}
+
+	if (ImGui::Button("TestLevel") && Level.get() != GetLevel())
+	{
+		GameEngineCore::ChangeLevel("TestLevel");
+	}
 }
 
