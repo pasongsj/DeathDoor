180c1eab32bf2553405e5d888d8b769f93c9d6e7
동찬 황 <shidge98@gmail.com>
2023-08-11 18:10:10 +0900

    황동찬 add 파일 업로드 아직 링크 미완성

--- GameEngineCore/PrecompileHeader.h
+++ GameEngineCore/PrecompileHeader.h
@@ -3,3 +3,16 @@
 #include <GameEnginePlatform/PrecompileHeader.h>
 
 #include "GameEngineDebug3D.h"
+
+
+#pragma warning(disable : 4996)
+#include <GameEngineCore/ThirdParty/PhysX/inc/PhysX/PxConfig.h>
+#include <GameEngineCore/ThirdParty/PhysX/inc/PhysX/PxPhysicsAPI.h>
+
+#pragma comment (lib, "PhysX_static_64.lib")
+#pragma comment (lib, "PhysXCharacterKinematic_static_64.lib")
+#pragma comment (lib, "PhysXCommon_static_64.lib")
+#pragma comment (lib, "PhysXExtensions_static_64.lib")
+#pragma comment (lib, "PhysXFoundation_static_64.lib")
+#pragma comment (lib, "PhysXPvdSDK_static_64.lib")
+#pragma comment (lib, "PhysXCooking_static_64.lib")
