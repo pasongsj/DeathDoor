78ccea82f1ffc6fdd4ab404cbf0da2ed7543b26f
동찬 황 <shidge98@gmail.com>
2023-08-16 03:32:41 +0900

    황동찬 refact 필요없다 생각되는 코드 주석처리 (Pvd) 및 정리

--- GameEngineContents/PhysXTestLevel.cpp
+++ GameEngineContents/PhysXTestLevel.cpp
@@ -15,15 +15,23 @@ PhysXTestLevel::PhysXTestLevel()
 
 PhysXTestLevel::~PhysXTestLevel() 
 {
-	Release();
 }
 
 void PhysXTestLevel::Start()
+{
+}
+
+void PhysXTestLevel::LevelChangeStart()
 {
 	GetMainCamera()->SetProjectionType(CameraType::Perspective);
 	GetMainCamera()->GetTransform()->SetLocalPosition({ 0, 0, -1000.0f });
 
 	Initialize();
+}
+
+void PhysXTestLevel::LevelChangeEnd()
+{
+	Release();
 }
 
 void PhysXTestLevel::Update(float _DeltaTime)
@@ -32,16 +40,16 @@ void PhysXTestLevel::Update(float _DeltaTime)
 }
 
 
-
+// 초기화
 void PhysXTestLevel::Initialize()
 {
 	m_pFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, m_Allocator, m_ErrorCallback);
 
-	m_pPvd = PxCreatePvd(*m_pFoundation);
-	physx::PxPvdTransport* pTransport = physx::PxDefaultPvdSocketTransportCreate(PVD_HOST, 5425, 10);
-	m_pPvd->connect(*pTransport, physx::PxPvdInstrumentationFlag::eALL);
+	//m_pPvd = PxCreatePvd(*m_pFoundation);
+	//physx::PxPvdTransport* pTransport = physx::PxDefaultPvdSocketTransportCreate(PVD_HOST, 54321, 1000);
+	//m_pPvd->connect(*pTransport, physx::PxPvdInstrumentationFlag::eALL);
 
-	m_pPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *m_pFoundation, physx::PxTolerancesScale(), true, m_pPvd);
+	m_pPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *m_pFoundation, physx::PxTolerancesScale(), true);
 
 	physx::PxSceneDesc SceneDesc(m_pPhysics->getTolerancesScale());
 	SceneDesc.gravity = physx::PxVec3(0.0f, -10.f, 0.0f);
@@ -50,43 +58,40 @@ void PhysXTestLevel::Initialize()
 	SceneDesc.filterShader = physx::PxDefaultSimulationFilterShader;
 	m_pScene = m_pPhysics->createScene(SceneDesc);
 
-	physx::PxPvdSceneClient* pPvdClient = m_pScene->getScenePvdClient();
-	if (pPvdClient)
-	{
-		pPvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
-		pPvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
-		pPvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
-	}
+	//physx::PxPvdSceneClient* pPvdClient = m_pScene->getScenePvdClient();
+	//if (pPvdClient)
+	//{
+	//	pPvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
+	//	pPvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
+	//	pPvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
+	//}
 
-	m_pMaterial = m_pPhysics->createMaterial(0.5f, 0.5f, 0.6f);
+	m_pMaterial = m_pPhysics->createMaterial(0.5f, 0.5f, 0.5f);
 
 	physx::PxRigidStatic* pGroundPlane = PxCreatePlane(*m_pPhysics, physx::PxPlane(0, 1, 0, 0), *m_pMaterial);
 	m_pScene->addActor(*pGroundPlane);
-
-	float4 tmpWorldPosition = {	pGroundPlane->getGlobalPose().p.x , pGroundPlane->getGlobalPose().p.y  , pGroundPlane->getGlobalPose().p.z };
-
 }
 
 
-// 실제로 물리연산을 실행
+// 물리연산
 void PhysXTestLevel::Simulate(bool _Value)
 {
 	m_pScene->simulate(1.0f / 60.0f);
 	m_pScene->fetchResults(_Value);
 }
 
-// 메모리제거
+// 메모리 해제
 void PhysXTestLevel::Release()
 {
 	PX_RELEASE(m_pScene);
 	PX_RELEASE(m_pDispatcher);
 	PX_RELEASE(m_pPhysics);
-	if (m_pPvd)
-	{
-		physx::PxPvdTransport* pTransport = m_pPvd->getTransport();
-		m_pPvd->release();	
-		m_pPvd = nullptr;
-		PX_RELEASE(pTransport);
-	}
+	//if (m_pPvd)
+	//{
+	//	physx::PxPvdTransport* pTransport = m_pPvd->getTransport();
+	//	m_pPvd->release();	
+	//	m_pPvd = nullptr;
+	//	PX_RELEASE(pTransport);
+	//}
 	PX_RELEASE(m_pFoundation);
 }
--- GameEngineContents/PhysXTestLevel.h
+++ GameEngineContents/PhysXTestLevel.h
@@ -18,22 +18,23 @@ public:
 protected:
 	void Start() override;
 	void Update(float _DeltaTime) override;
+
+	void LevelChangeStart() override;
+	void LevelChangeEnd() override;
 	
 private:
 	// Foundation을 생성하는데 필요한 변수
 	physx::PxDefaultAllocator		m_Allocator;
 	physx::PxDefaultErrorCallback	m_ErrorCallback;
 
-	physx::PxFoundation* m_pFoundation = nullptr;
-
 	physx::PxPhysics* m_pPhysics = nullptr;
-
-	physx::PxDefaultCpuDispatcher* m_pDispatcher = nullptr;
 	physx::PxScene* m_pScene = nullptr;
 	physx::PxMaterial* m_pMaterial = nullptr;
+	physx::PxFoundation* m_pFoundation = nullptr;
+	physx::PxDefaultCpuDispatcher* m_pDispatcher = nullptr;
 
 	physx::PxPvd* m_pPvd = nullptr;
-	// 클래스 초기화
+
 	void Initialize();
 
 	void Simulate(bool _Value = true);
