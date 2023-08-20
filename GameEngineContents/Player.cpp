a3b60a10ee7c60f3664d06a92f3bdfd19f825df6
EuihyeonO <yuu5666@naver.com>
2023-08-20 18:12:24 +0900

    오의현_feat : LevelFalling 오브젝트 배치 완료

--- GameEngineContents/Player.cpp
+++ GameEngineContents/Player.cpp
@@ -15,24 +15,22 @@ Player::~Player()
 {
 }
 
-
 void Player::Start()
 {
 	/*std::shared_ptr<GameEngineFBXRenderer> Renderer = CreateComponent<GameEngineFBXRenderer>();
 	Renderer->SetFBXMesh("House1.FBX", "MeshTexture");*/
 
 	std::shared_ptr<ContentFBXRenderer> Renderer2 = CreateComponent<ContentFBXRenderer>();
-	Renderer2->SetFBXMesh("Ocean.fbx", "ContentMesh");
+	Renderer2->SetFBXMesh("Stone_2x2.fbx", "ContentMesh");
 	auto Unit = Renderer2->GetUnTexturedUnit();
-	Unit[{0, 0}]->Color.MulColor = { 0.0f, 0.0f, 0.0f, 1 };
-	Unit[{0, 0}]->Color.PlusColor = { 0.35f, 0.58f, 0.74f, 1 };
+	Unit[{0, 0}]->Color.MulColor = { 0.0f, 0.0f, 0.0f, 0.0f };
+	Unit[{0, 0}]->Color.PlusColor = { 0.75f, 0.86f, 0.97f, 1 };
 
 	//Renderer2->GetTransform()->SetLocalPosition(float4(200, 0, 0));
 	float4 Scale = Renderer2->GetTransform()->GetLocalScale();
 	//Renderer2->GetTransform()->SetLocalScale(Scale * 10.0f);
 	// Renderer->SetFBXMesh("AnimMan.FBX", "MeshTexture", 0, 0);
 	// Renderer->SetFBXMesh("AnimMan.FBX", "MeshTexture", 0, 2);
-
 }
 
 void Player::Update(float _DeltaTime)
