4b9c430ca4ca00f6e111cf99b6a5517d119e0aef
허동석 <zest2700@gmail.com>
2023-10-11 23:16:06 +0900

    허동석_feat : 컬링관련 클래스 일부함수추가

--- GameEngineContents/CullingTrigger.cpp
+++ GameEngineContents/CullingTrigger.cpp
@@ -22,5 +22,16 @@ void CullingTrigger::Update(float _DeltaTime)
 }
 
 void CullingTrigger::InitComponent()
-{
+{	
+	m_pRenderer = CreateComponent<ContentFBXRenderer>();
+	// 네모난메쉬 아무거나 가져다 쓸거 
+	m_pRenderer->SetFBXMesh("Ladder.fbx", "ContentMeshDeffered");
+
+	float4 MeshScale = m_pRenderer->GetMeshScale();
+
+	m_pPhysXComponent = CreateComponent<PhysXBoxComponent>();
+	m_pPhysXComponent->SetPhysxMaterial(0.0f, 0.0f, 0.0f);
+	m_pPhysXComponent->CreatePhysXActors(MeshScale.PhysXVec3Return(), float4::ZERONULL, true);
+	m_pPhysXComponent->SetPositionSetFromParentFlag(true);
+	m_pPhysXComponent->SetFilterData(PhysXFilterGroup::Obstacle);
 }
