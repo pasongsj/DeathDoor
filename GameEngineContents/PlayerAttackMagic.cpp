0788f4d535829cb30af43a1e50af5c3dc6f96412
EuihyeonO <yuu5666@naver.com>
2023-10-27 14:15:35 +0900

    오의현_feat : 플레이어 Magic, Bomb 에 Particle추가

--- GameEngineContents/PlayerAttackMagic.cpp
+++ GameEngineContents/PlayerAttackMagic.cpp
@@ -4,6 +4,8 @@
 #include "ContentFBXRenderer.h"
 #include "PhysXSphereComponent.h"
 
+#include "DustParticle.h"
+
 PlayerAttackMagic::PlayerAttackMagic()
 {
 }
@@ -31,4 +33,21 @@ void PlayerAttackMagic::Start()
 void PlayerAttackMagic::Update(float _DeltaTime)
 {
 	AttackBase::Update(_DeltaTime);
+	CreateParticle(_DeltaTime);
+}
+
+void PlayerAttackMagic::CreateParticle(float _DeltaTime)
+{
+	ParticleCount += _DeltaTime;
+
+	if (ParticleCount >= 0.01f)
+	{
+		ParticleCount = 0.0f;
+
+		std::shared_ptr<DustParticle> NewParticle = CreateComponent<DustParticle>();
+		NewParticle->SetColor({ 233.0f / 255.0f, 77.0f / 255.0f });
+		NewParticle->GetTransform()->SetWorldPosition(GetTransform()->GetWorldPosition());
+		NewParticle->GetTransform()->SetLocalScale({ 50.0f, 50.0f, 50.0f });
+		NewParticle->SetGlow();
+	}
 }
