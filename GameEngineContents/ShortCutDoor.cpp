ce5b188bad2802d0cf51892acbae853d8a230a7d
동찬 황 <shidge98@gmail.com>
2023-10-30 01:18:35 +0900

    황동찬 : fix :  door 수정중

--- GameEngineContents/ShortCutDoor.cpp
+++ GameEngineContents/ShortCutDoor.cpp
@@ -48,10 +48,11 @@ void ShortCutDoor::InitAnimation()
 	m_pRenderer->CreateFBXAnimation("DROP", "SHORTCUTDOOR_DROP.FBX", { 1.f / 30.f, false });
 	m_pRenderer->CreateFBXAnimation("ROCKED", "SHORTCUTDOOR_ROCKED.FBX", { 1.f / 30.f, false });
 	m_pRenderer->CreateFBXAnimation("FLOOR", "SHORTCUTDOOR_FLOOR.FBX", { 1.f / 30.f, false });
-	m_pRenderer->CreateFBXAnimation("OPEN_Inward", "SHORTCUTDOOR_OPEN_INWARD.FBX", { 1.f / 30.f, true });
+	m_pRenderer->CreateFBXAnimation("OPEN_Inward", "SHORTCUTDOOR_OPEN_INWARD.FBX", { 1.f / 30.f, false });
 	m_pRenderer->CreateFBXAnimation("OPEN_STILL", "SHORTCUTDOOR_OPEN_STILL.FBX", { 1.f / 30.f, false });
 	m_pRenderer->CreateFBXAnimation("CLOSE_FROM_INWARD", "SHORTCUTDOOR_CLOSE_FROM_INWARD.FBX", { 1.f / 30.f, false });
-	m_pRenderer->ChangeAnimation("ROCKED"); 
+	m_pRenderer->ChangeAnimation("FLOOR"); 
+
 
 	//m_pRenderer1= CreateComponent<ContentFBXRenderer>();
 	//m_pRenderer1->GetTransform()->SetLocalScale(float4{ 100, 100, 100 });
@@ -73,6 +74,7 @@ void ShortCutDoor::InitAnimation()
 	//m_pRenderer1->SetUnitDiffuseColorIntensity(0, 0, 4.0f);
 }
 
+float test = 0.f;
 void ShortCutDoor::SetFSMFUNC()
 {
 	SetChangeFSMCallBack([this]
@@ -84,6 +86,20 @@ void ShortCutDoor::SetFSMFUNC()
 	SetFSM(TriggerState::OFF,
 		[this]
 		{
+			switch (m_eStartState)
+			{
+			case StartState::OPEN:
+			{
+				m_pRenderer->ChangeAnimation("OPEN_STILL");
+			}
+			break;
+			case StartState::CLOSE:
+			{
+				m_pRenderer->ChangeAnimation("FLOOR");
+			}
+			break;
+			}
+			m_pPhysXComponent->Death();
 		},
 		[this](float Delta)
 		{
@@ -104,21 +120,23 @@ void ShortCutDoor::SetFSMFUNC()
 			{
 			case StartState::OPEN:
 			{
-				m_pRenderer->ChangeAnimation("ROCKED");
+				m_pRenderer->ChangeAnimation("CLOSE_FROM_INWARD");
 			}
 				break;
 			case StartState::CLOSE:
 			{
-
+				m_pRenderer->ChangeAnimation("OPEN_Inward");
 			}
-				break;
-			default:
 				break;
 			}
 		},
 		[this](float Delta)
-		{
+		{	
 			
+			if (true == m_pRenderer->IsAnimationEnd())
+			{
+				SetNextState(TriggerState::ON);
+			}
 		},
 		[this]
 		{
@@ -128,12 +146,31 @@ void ShortCutDoor::SetFSMFUNC()
 	SetFSM(TriggerState::ON,
 		[this]
 		{
+			switch (m_eStartState)
+			{
+			case StartState::OPEN:
+			{
+				m_pRenderer->ChangeAnimation("FLOOR");
+			}
+			break;
+			case StartState::CLOSE:
+			{
+				m_pRenderer->ChangeAnimation("OPEN_STILL");
+			}
+			break;
+			}
+
 		},
 		[this](float Delta)
 		{
+			//È­¸é Fade?
 		},
 		[this]
 		{
+			if (nullptr != m_TriggerFunc)
+			{
+				m_TriggerFunc();
+			}
 		}
 	);
 }
