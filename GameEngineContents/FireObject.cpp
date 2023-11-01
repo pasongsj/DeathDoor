82c772c2a0054987c91972eec73fa0fcfcb80aa6
EuihyeonO <yuu5666@naver.com>
2023-11-01 13:19:03 +0900

    오의현_Feat : FireObject 빌보딩 해제

--- GameEngineContents/FireObject.cpp
+++ GameEngineContents/FireObject.cpp
@@ -43,41 +43,28 @@ void FireObject::Update(float _Delta)
 		NoiseData.FrameTime -= 10.0f;
 	}
 
-	BillBoarding();
-
-	//float4 CamPos = GetLevel()->GetMainCamera()->GetTransform()->GetWorldPosition();
-	//float4 FirePos = GetTransform()->GetWorldPosition();
-	//
-	//CamPos.y = 0.0f;
-	//FirePos.y = 0.0f;
-	//
-	//float4 Dir = CamPos - FirePos;
-	//Dir.Normalize();
-	//
-	//float4 DownLine = { 0.0f, 0.0f, -1.0f };
-	//
-	//float Radian = DownLine.x * Dir.x + DownLine.y * Dir.y + DownLine.z * Dir.z;
-	//float AcosRad = acos(Radian);
-	//float Degree = AcosRad * 180 / GameEngineMath::PIE;
-	//
-	//if (CamPos.x <= FirePos.x)
-	//{
-	//	GetTransform()->SetWorldRotation({ 0.0f, -90 + Degree, 0.0f });
-	//}
-	//else
-	//{
-	//	GetTransform()->SetWorldRotation({ 0.0f, -90 - Degree, 0.0f });
-	//}
+	//BillBoarding();
 }
 
 void FireObject::BillBoarding()
 {
-	float4x4 CamView = GetLevel()->GetMainCamera()->GetView();
-	float4 CamForward = GetLevel()->GetMainCamera()->GetTransform()->GetLocalForwardVector();
-
-	float4 BillBoardingAngle = CamForward + Rot;
-
-	GetTransform()->SetWorldRotation(BillBoardingAngle);
+	std::shared_ptr<GameEngineCamera> Cam = GetLevel()->GetMainCamera();
+	float4 CamRot = Cam->GetTransform()->GetWorldRotation();
+	float4x4 CamRotMatrix = CamRot.QuaternionToRotationMatrix();
+	
+	float4x4 RotMatrix;
+	RotMatrix.Identity();
+	
+	RotMatrix.Arr2D[0][0] = CamRotMatrix.Arr2D[0][0];
+	RotMatrix.Arr2D[0][2] = CamRotMatrix.Arr2D[0][2];
+	RotMatrix.Arr2D[2][0] = CamRotMatrix.Arr2D[2][0];
+	RotMatrix.Arr2D[2][2] = CamRotMatrix.Arr2D[2][2];
+	
+	float4 Rotation = float4::ZERO;
+	Rotation = Rotation.MatrixToQuaternion(CamRotMatrix.InverseReturn());
+	Rotation += Rot;
+	
+	GetTransform()->SetWorldRotation(Rotation);
 }
 
 
