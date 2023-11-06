fd540c6066a22712d1815f190db4492d8bc87837
EuihyeonO <yuu5666@naver.com>
2023-11-06 16:22:59 +0900

    오의현_Feat : HitParticle 구조 수정

--- GameEngineContents/HitParticle.h
+++ GameEngineContents/HitParticle.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "ParticleBase.h"
+#include <GameEngineBase/GameEngineRandom.h>
 
 class HitParticle : public ParticleBase
 {
@@ -15,10 +16,25 @@ public:
 	HitParticle& operator=(HitParticle&& _Other) noexcept = delete;
 
 	void Move(float _Delta);
-
+	void ScaleDecrease(float _Delta);
 	void SetDir(float4 _Dir)
 	{
 		Dir = _Dir;
+	}
+
+	void SetSpeed(float _Speed, float _Accel)
+	{
+		Speed = _Speed;
+		Accel = _Accel;
+	}
+
+	void SetScaleRange(float _Min, float _Max)
+	{
+		float ScaleX = GameEngineRandom::MainRandom.RandomFloat(_Min, _Max);
+
+		GetTransform()->SetWorldScale({ ScaleX, ScaleX / 10.0f });
+		FirstScale = ScaleX;
+		Scale = ScaleX;
 	}
 
 protected:
@@ -28,6 +44,7 @@ private:
 	std::shared_ptr<GameEngineRenderUnit> Unit;
 
 	float4 Dir = float4::ZERO;
+	float FirstScale = 0.0f;
 	float Scale = 0.0f;
 	float Speed = 10.0f;
 	float Accel = 10.0f;
