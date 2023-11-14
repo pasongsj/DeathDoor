6aeff4e2c84481ff8fc627158c858e93518205a7
EuihyeonO <yuu5666@naver.com>
2023-11-14 15:44:30 +0900

    오의현_feat : StartLevel 카메라 구도 수정 및 LevelChange 추가할 수 있도록 함수 추가

--- GameEngineContents/StartLevel.cpp
+++ GameEngineContents/StartLevel.cpp
@@ -15,18 +15,18 @@ void StartLevel::Start()
 {
 	SetLevelType(PacketLevelType::StartLevel);
 	GetMainCamera()->SetProjectionType(CameraType::Perspective);
-	GetMainCamera()->GetTransform()->SetLocalPosition({ 25.0f, 37.0f, -55.0f });
-	GetMainCamera()->GetTransform()->SetLocalRotation({ 28.0f, -30.0f, 0.0f });
+	GetMainCamera()->GetTransform()->SetLocalPosition({ 8.0f, 21.0f, -25.0f });
+	GetMainCamera()->GetTransform()->SetLocalRotation({ 32.0f, -30.0f, 0.0f });
 
 	GetCamera(100)->SetProjectionType(CameraType::Orthogonal);
 	GetCamera(100)->GetTransform()->SetLocalPosition({ 0.0f, 0.0f, 0.0f });
 
 	std::shared_ptr<GameEngineLight> Light = CreateActor<GameEngineLight>();
 	
-	Light->GetTransform()->SetWorldRotation({ 100.0f, 40.0f, 0.0f });
+	Light->GetTransform()->SetWorldRotation({ 60.0f, 40.0f, 0.0f });
 
 
-	AddPointLight({ .Color = {1.0f, 1.0f, 1.0f},.Position = {-24.0f, 12.0f, 6.0f},.MaxDist = 2.0f,.Intensity = 10.0f });
+	AddPointLight({ .Color = {1.0f, 1.0f, 1.0f},.Position = {-24.0f, 12.0f, 6.0f},.MaxDist = 3.0f,.Intensity = 10.0f });
 }
 
 void StartLevel::Update(float _DeltaTime)
